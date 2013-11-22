38e03515d263039cdbc5477b5705fe02ab708bf1
Bezumkin <bezumkin@yandex.ru>
2013-11-23 00:35:39 +0700

    Version 1.8.9-pl

--- _build/build.config.php
+++ _build/build.config.php
@@ -4,8 +4,8 @@
 define('PKG_NAME','pdoTools');
 define('PKG_NAME_LOWER',strtolower(PKG_NAME));
 
-define('PKG_VERSION','1.8.8');
-define('PKG_RELEASE','pl2');
+define('PKG_VERSION','1.8.9');
+define('PKG_RELEASE','pl');
 define('PKG_AUTO_INSTALL', true);
 
 
