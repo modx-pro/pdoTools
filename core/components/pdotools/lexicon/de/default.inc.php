2141e40d227fa2fd62a73b6505ef4650914cb493
Jan-Christoph Ihrens <ihrens@cc-services.de>
2018-02-06 21:12:26 +0100

    Completed / improved the German translation

--- core/components/pdotools/lexicon/de/default.inc.php
+++ core/components/pdotools/lexicon/de/default.inc.php
@@ -5,39 +5,41 @@
  * @package pdotools
  * @subpackage lexicon
  * @language de
- *
- * pdoTools translated to German by Jan-Christoph Ihrens (enigmatic_user, enigma@lunamail.de)
  */
 $_lang['pdotools'] = 'pdoTools';
 
-$_lang['setting_pdoTools.class'] = 'FQN der pdoTools';
-$_lang['setting_pdoTools.class_desc'] = 'Pfad für das laden von Klassen aus system-Einstellung "pdotools_class_path".';
-$_lang['setting_pdotools_class_path'] = 'Basispfad zu pdoTools-Klasse';
-$_lang['setting_pdotools_class_path_desc'] = 'Basispfad zu pdoTools Klasse, von dem es geladen wird, mit FQN.';
+$_lang['area_pdotools_main'] = 'Grundeinstellungen';
 
-$_lang['setting_pdoFetch.class'] = 'FQN der pdoFetch';
-$_lang['setting_pdoFetch.class_desc'] = 'Pfad für das laden von Klassen aus system-Einstellung "pdofetch_class_path".';
-$_lang['setting_pdofetch_class_path'] = 'Basispfad zu pdoFetch-Klasse';
-$_lang['setting_pdofetch_class_path_desc'] = 'Basispfad zu pdoFetch Klasse, von dem es geladen wird, mit FQN.';
+$_lang['setting_pdoTools.class'] = 'FQN von pdoTools';
+$_lang['setting_pdoTools.class_desc'] = 'FQN (Fully Qualified Name) der pdoTools-Klasse. Der Pfad für das Laden der Klasse wird in der Systemeinstellung "pdotools_class_path" festgelegt.';
+$_lang['setting_pdotools_class_path'] = 'Basispfad der pdoTools-Klasse';
+$_lang['setting_pdotools_class_path_desc'] = 'Basispfad der pdoTools-Klasse, aus dem sie bei Verwendung des FQN (Fully Qualified Name) geladen wird.';
 
-$_lang['setting_pdoParser.class'] = 'FQN der pdoParser';
-$_lang['setting_pdoParser.class_desc'] = 'Pfad für das laden von Klassen aus system-Einstellung "parser_class_path".';
+$_lang['setting_pdoFetch.class'] = 'FQN von pdoFetch';
+$_lang['setting_pdoFetch.class_desc'] = 'FQN (Fully Qualified Name) der pdoFetch-Klasse. Der Pfad für das Laden der Klasse wird in der Systemeinstellung "pdofetch_class_path" festgelegt.';
+$_lang['setting_pdofetch_class_path'] = 'Basispfad der pdoFetch-Klasse';
+$_lang['setting_pdofetch_class_path_desc'] = 'Basispfad der pdoFetch Klasse, aus dem sie bei Verwendung des FQN (Fully Qualified Name) geladen wird.';
+
+$_lang['setting_pdoParser.class'] = 'FQN von pdoParser';
+$_lang['setting_pdoParser.class_desc'] = 'FQN (Fully Qualified Name) der pdoParser-Klasse. Der Pfad für das Laden der Klasse wird in der Systemeinstellung "parser_class_path" festgelegt.';
 $_lang['setting_parser_class'] = 'Parser-Klasse';
-$_lang['setting_parser_class_desc'] = 'Parser-Klasse wird verwendet, um die MODX-tags.';
-$_lang['setting_parser_class_path'] = 'Der Pfad zum parser';
-$_lang['setting_parser_class_path_desc'] = 'Base Pfad zum parser-Klasse, von dem es geladen wird, mit FQN.';
+$_lang['setting_parser_class_desc'] = 'Parser-Klasse, die verwendet wird, um die MODX-Tags zu verarbeiten.';
+$_lang['setting_parser_class_path'] = 'Pfad zum Parser';
+$_lang['setting_parser_class_path_desc'] = 'Basispfad der Parser-Klasse, aus dem sie bei Verwendung des FQN (Fully Qualified Name) geladen wird.';
 
-$_lang['setting_pdotools_elements_path'] = 'Pfad der Elemente';
-$_lang['setting_pdotools_elements_path_desc'] = 'Verzeichnis mit Datei-Elemente zu laden via @FILE Bindung';
-$_lang['setting_pdotools_fenom_default'] = 'Verwenden Sie Fenom für chunks';
-$_lang['setting_pdotools_fenom_default_desc'] = 'pdoTools snippets verwenden Sie die Template-engine Fenom für chunk-Verarbeitung.';
-$_lang['setting_pdotools_fenom_parser'] = 'Verwenden Sie Fenom für Seiten';
-$_lang['setting_pdotools_fenom_parser_desc'] = 'pdoParser verwenden Sie die Template-engine Fenom bei der Verarbeitung Seiten und Website-Vorlagen. Es muss natürlich aktiviert sein.';
-$_lang['setting_pdotools_fenom_php'] = 'PHP in Fenom';
-$_lang['setting_pdotools_fenom_php_desc'] = 'Wenn aktiviert, können Sie PHP-Funktionen, die in den Vorlagen und Stücke, zum Beispiel {$.php.phpinfo()}.';
+$_lang['setting_pdotools_elements_path'] = 'Pfad zu den Elementen';
+$_lang['setting_pdotools_elements_path_desc'] = 'Verzeichnis mit Datei-Elementen, die mittels @FILE-Bindung geladen werden können.';
+$_lang['setting_pdotools_fenom_default'] = 'Fenom für Chunks verwenden';
+$_lang['setting_pdotools_fenom_default_desc'] = 'pdoTools-Snippets verwenden die Template-Engine Fenom für die Verarbeitung von Chunks.';
+$_lang['setting_pdotools_fenom_parser'] = 'Fenom für Seiten verwenden';
+$_lang['setting_pdotools_fenom_parser_desc'] = 'pdoParser verwendet die Template-Engine Fenom bei der Verarbeitung von Seiten und Templates. Sie muss dafür natürlich aktiviert sein.';
+$_lang['setting_pdotools_fenom_php'] = 'Erlaube PHP in Fenom';
+$_lang['setting_pdotools_fenom_php_desc'] = 'Wenn diese Einstellung aktiviert ist, können Sie PHP-Funktionen in Templates und Chunks nutzen, zum Beispiel {$.php.phpinfo()}.';
 $_lang['setting_pdotools_fenom_modx'] = 'Erlaube MODX in Fenom';
-$_lang['setting_pdotools_fenom_modx_desc'] = 'Diese Option ermöglicht die Verwendung von MODX und pdoTools in Fenom über {$modx} und {$pdoTools} Variablen.';
-$_lang['setting_pdotools_fenom_options'] = 'Fenom Optionen';
-$_lang['setting_pdotools_fenom_options_desc'] = 'JSON-string-array mit der beschriebenen Einstellungen auf <a href="https://github.com/fenom-Vorlage/fenom/blob/master/docs/de/Konfiguration.md" target="_blank">offiziellen Dokumentation</a>. Beispiel: {"auto_escape":true,"force_include":true}';
-$_lang['setting_pdotools_fenom_cache'] = 'Caching zusammengestellt Brocken';
-$_lang['setting_pdotools_fenom_cache_desc'] = 'Wenn Sie große und komplexe Fenom Brocken, können Sie die Zwischenspeicherung aktivieren der die kompilierten Versionen. Sie wird nur aktualisiert werden, wenn Sie die system-cache. Nicht empfohlen für die Entwicklung der Website.';
+$_lang['setting_pdotools_fenom_modx_desc'] = 'Diese Option ermöglicht die Verwendung von MODX und pdoTools in Fenom über die Variablen {$modx} und {$pdoTools}.';
+$_lang['setting_pdotools_fenom_options'] = 'Fenom-Optionen';
+$_lang['setting_pdotools_fenom_options_desc'] = 'JSON-String mit einem Array von Einstellungen, die in der <a href="https://github.com/fenom-Vorlage/fenom/blob/master/docs/de/Konfiguration.md" target="_blank">offiziellen Dokumentation</a> beschrieben werden. Beispiel: {"auto_escape": true, "force_include": true}';
+$_lang['setting_pdotools_fenom_cache'] = 'Caching verarbeiteter Chunks';
+$_lang['setting_pdotools_fenom_cache_desc'] = 'Wenn Sie große und komplexe Fenom-Chunks verwenden, können Sie das Caching der verarbeiteten Versionen aktivieren. Diese werden nur aktualisiert, wenn Sie den System-Cache leeren. Nicht empfohlen während der Entwicklung der Website.';
+$_lang['setting_pdotools_fenom_save_on_errors'] = 'Fehlermeldungen speichern';
+$_lang['setting_pdotools_fenom_save_on_errors_desc'] = 'Aktivieren Sie diese Einstellung, um die Fenom-Compiler-Fehlermeldungen im Verzeichnis "core/cache/default/pdotools/error" zum späteren Debuggen zu speichern.';
--- core/components/pdotools/lexicon/de/pdopage.inc.php
+++ core/components/pdotools/lexicon/de/pdopage.inc.php
@@ -5,11 +5,9 @@
  * @package pdotools
  * @subpackage lexicon
  * @language de
- *
- * pdoTools translated to German by Jan-Christoph Ihrens (enigmatic_user, enigma@lunamail.de)
  */
 $_lang['pdopage_first'] = 'Erste';
 $_lang['pdopage_last'] = 'Letzte';
 $_lang['pdopage_more'] = 'Laden';
-$_lang['pdopage_page'] = 'seite';
-$_lang['pdopage_from'] = 'aus';
+$_lang['pdopage_page'] = 'Seite';
+$_lang['pdopage_from'] = 'von';
