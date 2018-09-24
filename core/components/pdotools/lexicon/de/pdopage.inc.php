21097d5f517787f14318f23934ab8b8d802de87c
Sebastian G. Marinescu <sebastian-marinescu@users.noreply.github.com>
2018-09-24 14:52:05 +0200

    Update German "load more"

--- core/components/pdotools/lexicon/de/pdopage.inc.php
+++ core/components/pdotools/lexicon/de/pdopage.inc.php
@@ -8,6 +8,6 @@
  */
 $_lang['pdopage_first'] = 'Erste';
 $_lang['pdopage_last'] = 'Letzte';
-$_lang['pdopage_more'] = 'Laden';
+$_lang['pdopage_more'] = 'Mehr laden';
 $_lang['pdopage_page'] = 'Seite';
 $_lang['pdopage_from'] = 'von';
