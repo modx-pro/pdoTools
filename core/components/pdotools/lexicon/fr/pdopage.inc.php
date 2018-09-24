4553db31d320f74e490974315461f06111dd3363
Sebastian G. Marinescu <sebastian-marinescu@users.noreply.github.com>
2018-09-24 14:50:59 +0200

    Create pdopage.inc.php
    Add french pdoPage lexicon

---
+++ core/components/pdotools/lexicon/fr/pdopage.inc.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * pdoPage French Lexicon Entries for pdoPage
+ *
+ * @package pdotools
+ * @subpackage lexicon
+ * @language fr
+ */
+$_lang['pdopage_first'] = 'Première';
+$_lang['pdopage_last'] = 'Dernière';
+$_lang['pdopage_more'] = 'Charger plus';
+$_lang['pdopage_page'] = 'page';
+$_lang['pdopage_from'] = 'sur';
