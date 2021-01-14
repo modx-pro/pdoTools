3a06d29a0089ef951c82bbcb510cc6a3eefa121b
Vasily Naumkin <bezumkin@yandex.ru>
2021-01-14 10:13:29 +0700

    - Update Fenom to version 2.12.0
    Version 2.12.9-pl

--- _build/build.config.php
+++ _build/build.config.php
@@ -4,7 +4,7 @@
 define('PKG_NAME', 'pdoTools');
 define('PKG_NAME_LOWER', strtolower(PKG_NAME));
 
-define('PKG_VERSION', '2.12.8');
+define('PKG_VERSION', '2.12.9');
 define('PKG_RELEASE', 'pl');
 define('PKG_AUTO_INSTALL', true);
 
