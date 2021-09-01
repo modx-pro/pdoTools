db96246c5713e5d2a40d8156c807616b9849ab74
sergant210 <sergant210@bk.ru>
2021-09-01 10:13:49 +0300

    The settings "parser_class" and "parser_class_path" will be registered in the core namespace.

--- _build/resolvers/resolve.parser.php
+++ _build/resolvers/resolve.parser.php
@@ -2,50 +2,48 @@
 
 if ($object->xpdo) {
     /** @var $modx modX */
-    $modx =& $object->xpdo;
+    $modx = $object->xpdo;
 
     switch ($options[xPDOTransport::PACKAGE_ACTION]) {
         case xPDOTransport::ACTION_INSTALL:
         case xPDOTransport::ACTION_UPGRADE:
             /** @var modSystemSetting $tmp */
-            if (!$tmp = $modx->getObject('modSystemSetting', array('key' => 'parser_class'))) {
+            if (!$tmp = $modx->getObject('modSystemSetting', ['key' => 'parser_class'])) {
                 $tmp = $modx->newObject('modSystemSetting');
             }
-            $tmp->fromArray(array(
-                'namespace' => 'pdotools',
-                'area' => 'pdotools_main',
+            $tmp->fromArray([
+                'namespace' => 'core',
+                'area' => 'site',
                 'xtype' => 'textfield',
                 'value' => 'pdoParser',
                 'key' => 'parser_class',
-            ), '', true, true);
+            ], '', true, true);
             $tmp->save();
 
             /** @var modSystemSetting $tmp */
-            if (!$tmp = $modx->getObject('modSystemSetting', array('key' => 'parser_class_path'))) {
+            if (!$tmp = $modx->getObject('modSystemSetting', ['key' => 'parser_class_path'])) {
                 $tmp = $modx->newObject('modSystemSetting');
             }
-            $tmp->fromArray(array(
-                'namespace' => 'pdotools',
-                'area' => 'pdotools_main',
+            $tmp->fromArray([
+                'namespace' => 'core',
+                'area' => 'site',
                 'xtype' => 'textfield',
                 'value' => '{core_path}components/pdotools/model/pdotools/',
                 'key' => 'parser_class_path',
-            ), '', true, true);
+            ], '', true, true);
             $tmp->save();
 
             // Remove old settings
-            if ($tmp = $modx->getObject('modSystemSetting', array('key' => 'pdotools_useFenom'))) {
+            if ($tmp = $modx->getObject('modSystemSetting', ['key' => 'pdotools_useFenom'])) {
                 $tmp->remove();
             }
             break;
 
         case xPDOTransport::ACTION_UNINSTALL:
-            if ($tmp = $modx->getObject('modSystemSetting', array('key' => 'parser_class', 'value' => 'pdoParser'))) {
+            if ($tmp = $modx->getObject('modSystemSetting', ['key' => 'parser_class', 'value' => 'pdoParser'])) {
                 $tmp->remove();
             }
-            if ($tmp = $modx->getObject('modSystemSetting',
-                array('key' => 'parser_class_path', 'value' => '{core_path}components/pdotools/model/pdotools/'))
-            ) {
+            if ($tmp = $modx->getObject('modSystemSetting', ['key' => 'parser_class_path', 'value' => '{core_path}components/pdotools/model/pdotools/'])) {
                 $tmp->remove();
             }
             break;
--- core/components/pdotools/model/pdotools/pdofetch.class.php
+++ core/components/pdotools/model/pdotools/pdofetch.class.php
@@ -341,7 +341,7 @@ class pdoFetch extends pdoTools
             $this->addTime('Parameter "return" set to "ids", so we select only primary key', microtime(true) - $time);
         } elseif ($tmp = $this->config['select']) {
             if (!is_array($tmp)) {
-                $tmp = (!empty($tmp) && $tmp[0] == '{' || $tmp[0] == '[')
+                $tmp = (!empty($tmp) && ($tmp[0] == '{' || $tmp[0] == '['))
                     ? json_decode($tmp, true)
                     : array($this->config['class'] => $tmp);
             }
@@ -1123,7 +1123,7 @@ class pdoFetch extends pdoTools
         }
         $options['return'] = 'ids';
 
-        if ($id !== null && intval($depth) >= 1) {
+        if ($id !== null && (int)$depth >= 1) {
             $where[$parent_field] = (int)$id;
             $children = $this->getCollection($class, $where, $options);
             foreach ($children as $child) {
