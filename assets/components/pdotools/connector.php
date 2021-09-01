682d12fc5c5c7a561ea66833e98a390a6f772234
sergant210 <sergant210@bk.ru>
2021-09-01 10:55:57 +0300

    Update the ajax connector.

--- assets/components/pdotools/connector.php
+++ assets/components/pdotools/connector.php
@@ -1,16 +1,17 @@
 <?php
-/**@var modX $modx */
-define('MODX_API_MODE', true);
-require_once dirname(dirname(dirname(dirname(__FILE__)))) . '/index.php';
 
-$modx->getService('error', 'error.modError');
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config.core.php';
+require_once MODX_CORE_PATH . 'model/modx/modx.class.php';
+$modx = new modX();
+$modx->initialize('web');
+$modx->getService('error','error.modError', '', '');
 $modx->setLogLevel(modX::LOG_LEVEL_ERROR);
 $modx->setLogTarget('FILE');
 
 // Switch context if needed
 if (!empty($_REQUEST['pageId'])) {
-    if ($resource = $modx->getObject('modResource', (int)$_REQUEST['pageId'])) {
-        if ($resource->get('context_key') != 'web') {
+    if ($resource = $modx->getObject('modResource', ['id' => (int)$_REQUEST['pageId']])) {
+        if ($resource->get('context_key') !== 'web') {
             $modx->switchContext($resource->get('context_key'));
         }
         $modx->resource = $resource;
