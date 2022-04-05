69e1b14dbfe736f5a56603ec0fd7817197c59cc9
Adam Schaefers <schaefers@riseup.net>
2022-04-05 12:17:22 -0600

    [connector.php] Concat /config.core.php using dirname()

--- assets/components/pdotools/connector.php
+++ assets/components/pdotools/connector.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once $_SERVER['DOCUMENT_ROOT'] . '/config.core.php';
+require_once dirname(dirname(dirname(dirname(__FILE__)))) . '/config.core.php';
 require_once MODX_CORE_PATH . 'model/modx/modx.class.php';
 $modx = new modX();
 $modx->initialize('web');
