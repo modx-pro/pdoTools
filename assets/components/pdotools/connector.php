6965ac9e299ef2e3b7e31b9aa26ad77a56bf3a10
sshlokov <sshlokov@synergy.ru>
2022-07-25 11:01:52 +0300

    Changed $_SERVER['DOCUMENT_ROOT'] to dirname in the connector (#332).

--- assets/components/pdotools/connector.php
+++ assets/components/pdotools/connector.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once $_SERVER['DOCUMENT_ROOT'] . '/config.core.php';
+require_once dirname(__FILE__, 4) . '/config.core.php';
 require_once MODX_CORE_PATH . 'model/modx/modx.class.php';
 $modx = new modX();
 $modx->initialize('web');
